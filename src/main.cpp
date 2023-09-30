75331a8daac03803751a20cecf640d326925c11e
Finn Johnsen <finn.johnsen@kodemaker.no>
2023-09-30 20:13:52 +0200

    fikling med pattern

--- src/main.cpp
+++ src/main.cpp
@@ -2,64 +2,92 @@
 #include "easing.h"
 #include <FastLED.h>
 #include <sstream>
+#include <random>
 #include "ota.h"
 
 #define NUM_LEDS 71 
 #define LED_STRIP1_PIN GPIO_NUM_16
 #define LED_STRIP2_PIN GPIO_NUM_17
 
-CHSV ledHSV[NUM_LEDS];
-CRGB leds[NUM_LEDS];
-uint8_t hue = 0;
-
-
-uint8_t palIdx = 0;
-
-
-uint8_t pos = 0;
-
+CRGB strip_1[NUM_LEDS];
+CRGB strip_2[NUM_LEDS];
 
 void setup()
 {
     Serial.begin(9600);
     Serial.println("Starter");
-    FastLED.addLeds<WS2812B, LED_STRIP1_PIN, GRB>(leds, NUM_LEDS);
-    FastLED.addLeds<WS2812B, LED_STRIP2_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812B, LED_STRIP1_PIN, GRB>(strip_1, NUM_LEDS);
+    FastLED.addLeds<WS2812B, LED_STRIP2_PIN, GRB>(strip_2, NUM_LEDS);
 
-    FastLED.setBrightness(255);
     //FastLED.setBrightness(80);
     FastLED.setCorrection(UncorrectedColor);
+    FastLED.setBrightness(20);
 
     FastLED.setMaxPowerInVoltsAndMilliamps(5, 1500);
     OTA::setup();
     
 }
 
+CRGB colors[] = {
+    CRGB::PaleTurquoise,
+    CRGB::Pink,
+    CRGB::MidnightBlue,
+    CRGB::Chocolate,
+    CRGB::SpringGreen,
+};
+
+CRGB randomColor() {
+    int size = sizeof(colors) / sizeof(colors[0]);
+    auto randomIndex = rand() % size;
+    return colors[randomIndex];
+}
+
+bool firstRun = true;
+
+
 void loop()
 {
 
-    EVERY_N_MILLIS(30) {
+    EVERY_N_MILLIS(50) {
         //fadeUsingColor(leds, NUM_LEDS, ColorFromPalette(nor, 9, 190, NOBLEND));
         //fadeLightBy(leds, NUM_LEDS, 90);
-        fadeToBlackBy(leds, NUM_LEDS, 10);
+        fadeToBlackBy(strip_1, NUM_LEDS, 3);
+        fadeToBlackBy(strip_2, NUM_LEDS, 3);
+
     }
     
-    EVERY_N_MILLIS(3000) {
+    if (firstRun) {
+        firstRun = false;
+        int randInterval = 6+ (rand() % 10);
+
         for (int i = 0; i < NUM_LEDS; i++) {
-        if (i%10 == 0) {
-            leds[i] = CRGB::Red;
-
-        }
+            if (i%randInterval == 0) {
+                strip_1[i] = randomColor();
+                strip_2[i] = randomColor();
+            }
         //leds[i] = ledHSV[i];
+        }
     }
+
+    EVERY_N_MILLIS(5000) {
+        int randInterval = 6+ (rand() % 10);
+        int randInterval2 = 6+ (rand() % 10);
+
+        for (int i = 0; i < NUM_LEDS; i++) {
+            if (i%randInterval == 0) {
+                strip_1[i] = randomColor();
+            }
+             if (i%randInterval2 == 0) {
+                strip_2[i] = randomColor();
+            }
+        //leds[i] = ledHSV[i];
+        }
     }
-
-
-
+    
+    FastLED.setBrightness(20);
     FastLED.show();
 
-
-    EVERY_N_MILLIS(100) {
+    EVERY_N_MILLIS(300) {
         OTA::handle();
     }
 
